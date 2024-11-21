a0f6c6dc8e91f1af1840d28f09d6fb3cf215abca
dvillas28 <dvillasc@gmail.com>
2024-11-21 10:33:36 -0300

    iteraciones con variacion de N

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from os import path
 import pandas as pd
 from datos.parametros import cargar_parametros
 
-def main():
+def main(N: int, display_output: bool = False):
     
 ############## Carga de parametros y definicion de conjuntos #######################################
     
@@ -17,6 +17,9 @@ def main():
     
     model = Model("Problema de Optimizacion")
     model.setParam("TimeLimit", 30) # tiempo max de ejecución (en segundos)
+    
+    if not display_output:
+        model.setParam('OutputFlag', 0)
 
 ############## Variables de decision ############################################################### 
     
@@ -50,7 +53,7 @@ def main():
 ############## Restricciones #######################################################################
     
     # R2. Conservación del flujo: Todas las personas deben estar en un camino
-    model.addConstr(quicksum(X[i] for i in I) == params["N"], name="R2")
+    model.addConstr(quicksum(X[i] for i in I) == N, name="R2")
     
     # añadimos todas las demas restricciones del tipo "para todo i en I" en un solo loop (eficiente!)
     for i in I:
@@ -66,14 +69,14 @@ def main():
     
         # R4. Saturación: Si la cantidad de personas en un camino supera el umbral que permite la ruta 
         # sin saturarse, la ruta se satura (ocasionando S[i] = 1).
-        model.addConstr(X[i] <= c[i] * params["beta"] * (1 - S[i]) + params["N"] * S[i], name=f"R4_{i}")
+        model.addConstr(X[i] <= c[i] * params["beta"] * (1 - S[i]) + N * S[i], name=f"R4_{i}")
 
         # R5. Ruta en uso:  Si no hay personas en una ruta, esta no se encuentra        # sin saturarse, la ruta se satura (ocasionando S[i] = 1).
-        model.addConstr(R[i] * params["N"] >= X[i] , name=f"R5_{i}")
+        model.addConstr(R[i] * N >= X[i] , name=f"R5_{i}")
 
         # R6. Cumple calidad mínima: Si una calle no cumple el estándar mínimo de calidad, esta no se
         # Por lo cual su tiempo no será contabilizado en el cálculo de la función a minimizar.
-        model.addConstr(R[i] * params["N"] >= X[i] , name=f"R5_{i}")
+        model.addConstr(R[i] * N >= X[i] , name=f"R5_{i}")
 
         # R6. Cumple calidad mínima: Si una calle no cumple el estándar mínimo de calidad, esta no se
         # considera como una calle apta para el uso.
@@ -118,17 +121,28 @@ def main():
 
 ############### Impresion de resultados ############################################################
     
-    print(f"Valor de la funcion objetivo = {model.ObjVal}")
+    print(f"N = {N}")
+    try:
+        print(f"Funcion objetivo = {model.ObjVal}")
+    except:
+        print("Modelo infactible: No se encontro una solucion")
+        return
+    
+    # calcular el numero de T_i != 0
+    num_ti = sum(1 for i in I if X[i].x != 0)
+    print("n° de rutas ocupadas = ", num_ti)
+    print(f"Tiempo por ruta = {model.ObjVal / num_ti}")
 
-    for key, value in params.items():
-        print(f"{key}: {value}")
+    if display_output:
+        for key, value in params.items():
+            print(f"{key}: {value}")
 
-    # Imprimir el encabezado de la tabla con formato alineado
-    print(f"{'ruta':<6} | {'X_i':<4} | {'T_i':<6} | {'R_i':<5} | {'Theta_i':<8} | {'A_i':<5} | {'S_i':<5} | {'Q_i':<5} | {'nombre'} ")
+        # Imprimir el encabezado de la tabla con formato alineado
+        print(f"{'ruta':<6} | {'X_i':<4} | {'T_i':<6} | {'R_i':<5} | {'Theta_i':<8} | {'A_i':<5} | {'S_i':<5} | {'Q_i':<5} | {'nombre'} ")
 
-    # Iterar sobre los elementos de I y mostrar los valores con el mismo formato alineado
-    for i in I:
-        print(f"{i:<6} |{X[i].x:<5} | {round(T[i].x, 2):<6} | {R[i].x:<5} | {Theta[i].x:<8} | {A[i].x:<5} | {S[i].x:<5} | {Q[i].x:<5} | {names[i]}")
+        # Iterar sobre los elementos de I y mostrar los valores con el mismo formato alineado
+        for i in I:
+            print(f"{i:<6} |{X[i].x:<5} | {round(T[i].x, 2):<6} | {R[i].x:<5} | {Theta[i].x:<8} | {A[i].x:<5} | {S[i].x:<5} | {Q[i].x:<5} | {names[i]}")
 
     
     # Escribir a un excel los resultados
@@ -136,19 +150,34 @@ def main():
     redondear = lambda var: round(var.x, 2)
 
     data = {
-        "ruta": pd.Series(names),
-        "X_i":pd.Series(map(redondear, X.values())),
-        "T_i":pd.Series(map(redondear, T.values())),
-        "R_i":pd.Series(map(redondear, R.values())),
-        "Theta_i":pd.Series(map(redondear, Theta.values())),
-        "A_i":pd.Series(map(redondear, A.values())),
-        "S_i":pd.Series(map(redondear, S.values())),
-        "Q_i":pd.Series(map(redondear, Q.values()))
+        "Nombre ruta": pd.Series(names),
+        "¿Se utiliza esa ruta? (R_i)":pd.Series(map(redondear, R.values())),
+        "Cantidad de personas que evacuan (X_i)":pd.Series(map(redondear, X.values())),
+        "Tiempo de evacuación por esa ruta (T_i)":pd.Series(map(redondear, T.values())),
+        "¿Sobre pasa la inclinacion maxima? (Theta_i)":pd.Series(map(redondear, Theta.values())),
+        "¿Cumple con el ancho minimo? (A_i)":pd.Series(map(redondear, A.values())),
+        "¿Cumple con la calidad minima? (Q_i)":pd.Series(map(redondear, Q.values())),
+        "¿Se satura la ruta? (S_i)":pd.Series(map(redondear, S.values()))
     }
 
     df = pd.DataFrame(data)
-    df.to_excel(path.join("resultados", "resultados.xlsx"), index=False)
+    df.to_excel(path.join("resultados", "sensibilidad", "N", f"resultados_N_{N}.xlsx"), index=False)
 
 
 if __name__ == "__main__":
-    main()
+    # lista del 10000 hasta el 1000 de 1000 en 1000
+    lista_N = list(range(10000, 0, -1000))
+    lista_N.append(500)
+    lista_N.append(250)
+    lista_N.append(100)
+    lista_N.append(50)
+    lista_N.append(10)
+    lista_N.append(1)
+    lista_N.append(11000)
+    
+    for n in lista_N:
+        print("---------------------------------------------------")
+        main(n)
+        print("---------------------------------------------------")
+
+    # main(10000, True)
